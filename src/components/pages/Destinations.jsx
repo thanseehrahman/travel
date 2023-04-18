4f90c984bfe9f8c06f0ec13d2c6a290ae3e82863
Thanseeh Rahman <thanseehrahman004@gmail.com>
2023-04-19 02:21:06 +0530

    Responsive added

--- src/components/pages/Destinations.jsx
+++ src/components/pages/Destinations.jsx
@@ -20,6 +20,17 @@ const Container = styled.div`
   padding: 60px 60px;
   position: relative;
   top: 140px;
+
+  @media (max-width: 1440px) {
+    top: 105px;
+  }
+  @media (max-width: 768px) {
+    top: 96px;
+  }
+  @media (max-width: 480px) {
+    padding: 60px 32px 32px;
+    top: 62px;
+  }
 `;
 
 const Wrap = styled.div`
@@ -31,6 +42,16 @@ const Heading = styled.h1`
   font-size: 70px;
   font-weight: 600;
   margin-bottom: 20px;
+
+  @media (max-width: 980px) {
+    font-size: 62px;
+  }
+  @media (max-width: 640px) {
+    font-size: 48px;
+  }
+  @media (max-width: 480px) {
+    font-size: 38px;
+  }
 `;
 
 const Description = styled.p`
