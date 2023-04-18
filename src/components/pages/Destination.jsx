2d64dd05b2a2a933f81aebe1d4cb33628972b001
Thanseeh Rahman <thanseehrahman004@gmail.com>
2023-04-19 00:51:09 +0530

    Responsive added

--- src/components/pages/Destination.jsx
+++ src/components/pages/Destination.jsx
@@ -97,6 +97,17 @@ const Container = styled.div`
   padding: 60px 60px;
   position: relative;
   top: 140px;
+
+  @media (max-width: 1440px) {
+    top: 105px;
+  }
+  @media (max-width: 768px) {
+    top: 96px;
+  }
+  @media (max-width: 480px) {
+    padding: 60px 32px 32px;
+    top: 62px;
+  }
 `;
 
 const Wrap = styled.div`
@@ -108,29 +119,59 @@ const Heading = styled.h1`
   font-size: 70px;
   font-weight: 600;
   margin-bottom: 20px;
+
+  @media (max-width: 980px) {
+    font-size: 62px;
+  }
+  @media (max-width: 640px) {
+    font-size: 48px;
+  }
+  @media (max-width: 480px) {
+    font-size: 38px;
+  }
 `;
 
 const Labels = styled.div`
   display: flex;
   align-items: center;
+  gap: 20px;
   margin-bottom: 30px;
+
+  @media (max-width: 640px) {
+    flex-direction: column;
+    align-items: start;
+    gap: 10px;
+  }
 `;
 
 const Location = styled.div`
   display: flex;
   align-items: center;
-  margin-right: 20px;
+
+  @media (max-width: 640px) {
+    padding: 8px 12px;
+    background: #e6e6e6;
+    border-radius: 8px;
+  }
 `;
 
 const Icon = styled.img`
   width: 24px;
   display: inline-block;
   margin-right: 5px;
+
+  @media (max-width: 640px) {
+    width: 20px;
+  }
 `;
 
 const Title = styled.h6`
   font-size: 18px;
   font-weight: 500;
+
+  @media (max-width: 640px) {
+    font-size: 16px;
+  }
 `;
 
 const Category = styled(Location)``;
@@ -140,6 +181,16 @@ const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 30px;
+
+  @media (max-width: 980px) {
+    height: 310px;
+    display: flex;
+    overflow-x: scroll;
+    scroll-snap-type: x mandatory;
+  }
+  @media (max-width: 640px) {
+    height: 240px;
+  }
 `;
 
 const MainImage = styled.div`
@@ -147,21 +198,47 @@ const MainImage = styled.div`
   border-radius: 20px;
   grid-column: span 2;
   grid-row: span 2;
+
+  @media (max-width: 980px) {
+    width: 300px;
+    flex: none;
+    scroll-snap-align: start;
+  }
+  @media (max-width: 640px) {
+    width: 230px;
+  }
 `;
 
 const Image = styled.div`
   height: 100%;
   border-radius: 20px;
+
+  @media (max-width: 980px) {
+    width: 300px;
+    flex: none;
+    scroll-snap-align: start;
+  }
+  @media (max-width: 640px) {
+    width: 230px;
+  }
 `;
 
 const Content = styled.div`
   padding: 60px 0 0;
+
+  @media (max-width: 480px) {
+    padding: 32px 0 0;
+  }
 `;
 
 const SubHeading = styled.h3`
   font-size: 28px;
   font-weight: 500;
   margin-bottom: 20px;
+
+  @media (max-width: 640px) {
+    font-size: 20px;
+  }
 `;
 
 const Description = styled.p`
