9e5b24f6a16fc8dcf352ef849e898e2065501aaa
Thanseeh Rahman <thanseehrahman004@gmail.com>
2023-04-18 12:00:03 +0530

    Data provider connected

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Header from "./components/Header";
+import { DataContextProvider } from "./context/DataContext";
 
 function App() {
   return (
-    <>
+    <DataContextProvider>
       <Header />
       <Routes>
         <Route element={<Home />} exact path="/" />
       </Routes>
-    </>
+    </DataContextProvider>
   );
 }
 
