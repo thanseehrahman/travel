08940d5bb93a7cc4b35449d429336e1f223ae2f5
Thanseeh Rahman <thanseehrahman004@gmail.com>
2023-04-19 01:34:34 +0530

    Side menu added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 function Header() {
+  const [sideMenuStatus, setSideMenuStatus] = useState(false);
+
   return (
     <Container>
       <TopMenu>
@@ -14,8 +16,65 @@ function Header() {
             <LinkTo to="/destinations">Destinations</LinkTo>
           </Navigation>
           <Button to="/destinations">Explore</Button>
+          <Hamburger
+            onClick={() => {
+              setSideMenuStatus(true);
+            }}
+          >
+            <Icon src="/images/icons/hamburger.svg" />
+          </Hamburger>
         </FlexWrap>
       </TopMenu>
+
+      <SideMenu
+        style={
+          sideMenuStatus
+            ? { transform: "translateX(0)" }
+            : { transform: "translateX(280px)" }
+        }
+      >
+        <Close
+          onClick={() => {
+            setSideMenuStatus(false);
+          }}
+        >
+          <Icon src="/images/icons/close.svg" />
+        </Close>
+        <SideNavigation>
+          <LinkTo
+            onClick={() => {
+              setSideMenuStatus(false);
+            }}
+            to="/"
+          >
+            Home
+          </LinkTo>
+          <LinkTo
+            onClick={() => {
+              setSideMenuStatus(false);
+            }}
+            to="/about"
+          >
+            About
+          </LinkTo>
+          <Destinations>
+            <Top>
+              <LinkTo
+                onClick={() => {
+                  setSideMenuStatus(false);
+                }}
+                to="/destinations"
+              >
+                Destinations
+              </LinkTo>
+              <Dropdown>
+                <SmallIcon src={"/images/icons/down.svg"} />
+              </Dropdown>
+            </Top>
+            <Bottom></Bottom>
+          </Destinations>
+        </SideNavigation>
+      </SideMenu>
     </Container>
   );
 }
@@ -89,6 +148,75 @@ const Button = styled(Link)`
   @media (max-width: 768px) {
     display: none;
   }
+`;
+
+const Hamburger = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    display: block;
+  }
+`;
+
+const Icon = styled.img`
+  height: 30px;
+  display: block;
+
+  @media (max-width: 640px) {
+    height: 20px;
+  }
+`;
+
+const SideMenu = styled.div`
+  height: 100vh;
+  width: 240px;
+  padding: 16px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  display: none;
+  background: inherit;
+  box-shadow: 4px 0px 40px rgba(0, 0, 0, 0.1);
+  transform: translateX(280px);
+  transition: transform 0.6s ease-in-out;
+
+  @media (max-width: 768px) {
+    display: block;
+  }
+`;
+
+const Close = styled.div`
+  margin-bottom: 30px;
+`;
+
+const SideNavigation = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const Destinations = styled.div``;
+
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 16px;
+`;
+
+const Dropdown = styled.div``;
+
+const SmallIcon = styled(Icon)`
+  height: 24px;
+
+  @media (max-width: 640px) {
+    height: 20px;
+  }
+`;
+
+const Bottom = styled.div`
+  display: none;
+  overflow: hidden;
 `;
 
 export default Header;
